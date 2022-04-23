3e58faa22a00f0758b0affb79e9f9f4a7c0b15e7
Fil <fil931@gmail.com>
2022-04-23 12:27:08 +0300

    Initial commit

---
+++ src/main/java/org/example/MyIterator.java
@@ -0,0 +1,45 @@
+package org.example;
+
+
+import com.sun.tools.javac.util.ArrayUtils;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyIterator <T> implements Iterator {
+    private T[] object;
+    private int index = 0;
+    private int length;
+
+
+    public MyIterator(T[] object) {
+        this.object = object;
+        this.length = object.length;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < length;
+    }
+
+    @Override
+    public T next() {
+        if(index < length) {
+            return  object[index++];
+        } else {
+            throw new NoSuchElementException("No elements remain");
+        }
+    }
+
+    @Override
+    public void remove() {
+        int tempLength = length-1;
+        T[] temp = (T[]) new Object[tempLength];
+        if (index >= 0) System.arraycopy(object, 0, temp, 0, index);
+        if (length + 1 - (index + 1) >= 0)
+            System.arraycopy(object, index + 1, temp, index + 1, length + 1 - (index + 1));
+        object = temp;
+        length = tempLength;
+    }
+}
